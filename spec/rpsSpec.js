176e7befaf2f6f73572667dfdd9ac2223c6ef04c
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-21 11:35:02 +0100

    wrote the first test

---
+++ spec/rpsSpec.js
@@ -0,0 +1,9 @@
+describe('Rock Paper Scissors', function(){
+
+	describe('Types of gestures', function(){
+
+		it('rock is a rock', function(){
+			expect(rock.type).toEqual('rock');
+		});
+	});
+});
